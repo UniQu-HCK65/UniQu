8b1313d4512c7d5ac3142f7c2419d4f73562a84b
Koso55 <radityoprakoso55@gmail.com>
2024-01-18 16:45:37 +0700

    feat added getUserById, add Review, bookings, get all transactions, user profile talent profile, added role guard on some endpoints  fix: profile user, profile talent, added some extra data to json

--- server/schemas/bookings.js
+++ server/schemas/bookings.js
@@ -1,149 +1,98 @@
-// const client = require("../config/configMongo");
-// const { GraphQLError } = require("graphql");
-// const { ObjectId } = require("mongodb");
+const client = require("../config/configMongo");
+const { GraphQLError } = require("graphql");
+const { ObjectId } = require("mongodb");
 
-// const { formatDate } = require("../helpers/dateFormat");
+const { formatDate } = require("../helpers/dateFormat");
 
-// const COLLECTION_NAME = "Follows";
+const COLLECTION_NAME = "Bookings";
 
-// // your data.
-// const typeDefs = `#graphql
-//   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+// your data.
+const typeDefs = `#graphql
 
-//   # This "Book" type defines the queryable fields for every book in our data source.
-//   type Follow {
-//     _id: ID
-//     followingId: ID
-//     followerId: ID
-//     createdAt: String
-//     updatedAt: String
-//   }
+  type Booking {
+    _id: ID
+    TalentId: ID
+    UserId: ID
+    talentName: String
+    userName: String
+    TransactionId: ID
+    bookDate: String
+    bookLocation: String
+    bookStatus: String
+    updatedAt: String
+    createdAt: String
+  }
 
-//   input NewFollow {
-//     followingId: ID!
-//     followerId: ID!
-//   }
+  input NewBooking {
+    TalentId: ID
+    TransactionId: ID
+    bookDate: String
+    bookLocation: String
+  }
 
-//   # The "Query" type is special: it lists all of the available queries that
-//   # clients can execute, along with the return type for each. In this
-//   # case, the "books" query returns an array of zero or more Books (defined above).
-//   type Query {
-//     follows: [Follow]
-//   }
+  type Query {
+    bookings: [Booking]
+  }
 
-//   type Mutation {
-//   follow(newFollow:NewFollow) : Follow
-//   }
-// `;
+  type Mutation {
+  book(newBooking:NewBooking) : Booking
+  }
+`;
 
-// // Resolvers define how to fetch the types defined in your schema.
-// // This resolver retrieves books from the "books" array above.
-// const resolvers = {
-//   Query: {
-//     follows: async (parent, args, contextValue, info) => {
-//       const { db, authentication } = contextValue;
-//       const auth = await authentication();
+const resolvers = {
+  Query: {
+    bookings: async (parent, args, contextValue, info) => {
+      try {
+        const { db, authentication } = contextValue;
+        const auth = await authentication();
+        const bookings = await db.collection(COLLECTION_NAME).find().toArray();
+        return bookings;
+      } catch (error) {
+        console.log(error, "GET_BOOKINGS"); // errorHandler next up
+        throw new GraphQLError(error.message || "Internal Server Error", {
+          extensions: {
+            code: error.code || "INTERNAL_SERVER_ERROR",
+            http: { status: error.status || 500 },
+          },
+        });
+      }
+    },
+  },
 
-//       const follows = await db.collection(COLLECTION_NAME).find().toArray();
+  Mutation: {
+    book: async (parent, args, contextValue, info) => {
+      //BELOM JALAN OK, FLOW
+      try {
+        const { newBooking } = args;
+        const { db, authentication } = contextValue;
 
-//       return follows;
-//     },
-//   },
+        const auth = await authentication();
 
-//   Mutation: {
-//     follow: async (parent, args, contextValue, info) => {
-//       try {
-//         const { newFollow } = args;
-//         const { db, authentication } = contextValue;
+        return {
+          _id: new ObjectId("1"),
+          TalentId: new ObjectId("1"),
+          UserId: new ObjectId(auth._id),
+          TransactionId: new ObjectId("a"),
+          bookDate: new Date(),
+          bookLocation: "String",
+          bookStatus: "String",
+          updatedAt: new Date(),
+          createdAt: new Date(),
+        };
+      } catch (error) {
+        console.log(error, "FOLLOW"); // errorHandler next up
+        throw new GraphQLError(error.message || "Internal Server Error", {
+          extensions: {
+            code: error.code || "INTERNAL_SERVER_ERROR",
+            http: { status: error.status || 500 },
+          },
+        });
+      }
+    },
+  },
+};
 
-//         const auth = await authentication();
-
-//         newFollow.followerId = auth._id;
-//         if (!newFollow.followerId)
-//           throw {
-//             message: "Can't follow, you are not logged in",
-//             code: "UNAUTHORIZED",
-//             status: 401,
-//           };
-
-//         if (!newFollow.followingId)
-//           throw {
-//             message: "UserId is not given",
-//             code: "BAD_REQUEST",
-//             status: 400,
-//           };
-
-//         if (newFollow.followerId === newFollow.followingId)
-//           throw {
-//             message: "You can't follow yourself",
-//             code: "FORBIDDEN",
-//             status: 403,
-//           };
-
-//         console.log(newFollow.followerId, "FOLLOWER");
-//         console.log(newFollow.followingId, "FOLLOWING");
-
-//         const follows = await db.collection(COLLECTION_NAME);
-
-//         const isFollowing = await follows
-//           .aggregate([
-//             {
-//               $match: {
-//                 followerId: new ObjectId(newFollow.followerId),
-//                 followingId: new ObjectId(newFollow.followingId),
-//               },
-//             },
-//           ])
-//           .toArray();
-
-//         console.log(isFollowing, "DISINI");
-
-//         if (isFollowing.length > 0)
-//           throw {
-//             message: "You already followed this user",
-//             code: "FORBIDDEN",
-//             status: 403,
-//           };
-
-//         const insertFollow = await follows.insertOne({
-//           ...newFollow,
-//           followerId: new ObjectId(newFollow.followerId),
-//           followingId: new ObjectId(newFollow.followingId),
-//           createdAt: new Date(),
-//           updatedAt: new Date(),
-//         });
-
-//         const followToShow = {
-//           followerId: new ObjectId(newFollow.followerId),
-//           followingId: new ObjectId(newFollow.followingId),
-//           createdAt: new Date(),
-//           updatedAt: new Date(),
-//         };
-
-//         // console.log(insertFollow, "FOLLOW SUCCESS");
-
-//         return {
-//           ...newFollow,
-//           _id: insertFollow.insertedId,
-//           followerId: followToShow.followerId,
-//           followingId: followToShow.followingId,
-//           createdAt: formatDate(followToShow.createdAt),
-//           updatedAt: formatDate(followToShow.updatedAt),
-//         };
-//       } catch (error) {
-//         console.log(error, "FOLLOW"); // errorHandler next up
-//         throw new GraphQLError(error.message || "Internal Server Error", {
-//           extensions: {
-//             code: error.code || "INTERNAL_SERVER_ERROR",
-//             http: { status: error.status || 500 },
-//           },
-//         });
-//       }
-//     },
-//   },
-// };
-
-// module.exports = {
-//   typeDefs,
-//   resolvers,
-// };
+module.exports = {
+  typeDefs,
+  resolvers,
+};
--- server/schemas/transactions.js
+++ server/schemas/transactions.js
@@ -1,149 +1,70 @@
-// const client = require("../config/configMongo");
-// const { GraphQLError } = require("graphql");
-// const { ObjectId } = require("mongodb");
+const client = require("../config/configMongo");
+const { GraphQLError } = require("graphql");
+const { ObjectId } = require("mongodb");
 
-// const { formatDate } = require("../helpers/dateFormat");
+const { formatDate } = require("../helpers/dateFormat");
 
-// const COLLECTION_NAME = "Follows";
+const COLLECTION_NAME = "Transactions";
 
-// // your data.
-// const typeDefs = `#graphql
-//   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+// your data.
+const typeDefs = `#graphql
 
-//   # This "Book" type defines the queryable fields for every book in our data source.
-//   type Follow {
-//     _id: ID
-//     followingId: ID
-//     followerId: ID
-//     createdAt: String
-//     updatedAt: String
-//   }
+  type Transaction {
+    _id: ID
+    TalentId: ID
+    UserId: ID
+    talentName: String
+    userName: String
+    paymentId: String
+    transactionStatus: String
+    paidByAdmin: Boolean
+    updatedAt: String
+    createdAt: String
+  }
 
-//   input NewFollow {
-//     followingId: ID!
-//     followerId: ID!
-//   }
+  input NewTransaction {
+    followingId: ID!
+    followerId: ID!
+  }
 
-//   # The "Query" type is special: it lists all of the available queries that
-//   # clients can execute, along with the return type for each. In this
-//   # case, the "books" query returns an array of zero or more Books (defined above).
-//   type Query {
-//     follows: [Follow]
-//   }
+  type Query {
+    transactions: [Transaction]
+  }
 
-//   type Mutation {
-//   follow(newFollow:NewFollow) : Follow
-//   }
-// `;
+`;
 
-// // Resolvers define how to fetch the types defined in your schema.
-// // This resolver retrieves books from the "books" array above.
-// const resolvers = {
-//   Query: {
-//     follows: async (parent, args, contextValue, info) => {
-//       const { db, authentication } = contextValue;
-//       const auth = await authentication();
+// type Mutation { //BELOM JALAN OK, FLOW
+// follow(newFollow:NewFollow) : Follow
+// }
 
-//       const follows = await db.collection(COLLECTION_NAME).find().toArray();
+const resolvers = {
+  Query: {
+    transactions: async (parent, args, contextValue, info) => {
+      try {
+        const { db, authentication } = contextValue;
+        const auth = await authentication();
 
-//       return follows;
-//     },
-//   },
+        const transactions = await db
+          .collection(COLLECTION_NAME)
+          .find()
+          .toArray();
+        return transactions;
+      } catch (error) {
+        console.log(error, "GET_TRANSACTIONS"); // errorHandler next up
+        throw new GraphQLError(error.message || "Internal Server Error", {
+          extensions: {
+            code: error.code || "INTERNAL_SERVER_ERROR",
+            http: { status: error.status || 500 },
+          },
+        });
+      }
+    },
+  },
 
-//   Mutation: {
-//     follow: async (parent, args, contextValue, info) => {
-//       try {
-//         const { newFollow } = args;
-//         const { db, authentication } = contextValue;
+  Mutation: {},
+};
 
-//         const auth = await authentication();
-
-//         newFollow.followerId = auth._id;
-//         if (!newFollow.followerId)
-//           throw {
-//             message: "Can't follow, you are not logged in",
-//             code: "UNAUTHORIZED",
-//             status: 401,
-//           };
-
-//         if (!newFollow.followingId)
-//           throw {
-//             message: "UserId is not given",
-//             code: "BAD_REQUEST",
-//             status: 400,
-//           };
-
-//         if (newFollow.followerId === newFollow.followingId)
-//           throw {
-//             message: "You can't follow yourself",
-//             code: "FORBIDDEN",
-//             status: 403,
-//           };
-
-//         console.log(newFollow.followerId, "FOLLOWER");
-//         console.log(newFollow.followingId, "FOLLOWING");
-
-//         const follows = await db.collection(COLLECTION_NAME);
-
-//         const isFollowing = await follows
-//           .aggregate([
-//             {
-//               $match: {
-//                 followerId: new ObjectId(newFollow.followerId),
-//                 followingId: new ObjectId(newFollow.followingId),
-//               },
-//             },
-//           ])
-//           .toArray();
-
-//         console.log(isFollowing, "DISINI");
-
-//         if (isFollowing.length > 0)
-//           throw {
-//             message: "You already followed this user",
-//             code: "FORBIDDEN",
-//             status: 403,
-//           };
-
-//         const insertFollow = await follows.insertOne({
-//           ...newFollow,
-//           followerId: new ObjectId(newFollow.followerId),
-//           followingId: new ObjectId(newFollow.followingId),
-//           createdAt: new Date(),
-//           updatedAt: new Date(),
-//         });
-
-//         const followToShow = {
-//           followerId: new ObjectId(newFollow.followerId),
-//           followingId: new ObjectId(newFollow.followingId),
-//           createdAt: new Date(),
-//           updatedAt: new Date(),
-//         };
-
-//         // console.log(insertFollow, "FOLLOW SUCCESS");
-
-//         return {
-//           ...newFollow,
-//           _id: insertFollow.insertedId,
-//           followerId: followToShow.followerId,
-//           followingId: followToShow.followingId,
-//           createdAt: formatDate(followToShow.createdAt),
-//           updatedAt: formatDate(followToShow.updatedAt),
-//         };
-//       } catch (error) {
-//         console.log(error, "FOLLOW"); // errorHandler next up
-//         throw new GraphQLError(error.message || "Internal Server Error", {
-//           extensions: {
-//             code: error.code || "INTERNAL_SERVER_ERROR",
-//             http: { status: error.status || 500 },
-//           },
-//         });
-//       }
-//     },
-//   },
-// };
-
-// module.exports = {
-//   typeDefs,
-//   resolvers,
-// };
+module.exports = {
+  typeDefs,
+  resolvers,
+};
