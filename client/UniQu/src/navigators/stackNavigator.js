4421172f0d0af1f0879c8264e9af5602cda03cc8
faishalam <faishalabdulmajid@gmail.com>
2024-01-18 13:39:45 +0700

    tabNavigator

--- client/UniQu/src/navigators/stackNavigator.js
+++ client/UniQu/src/navigators/stackNavigator.js
@@ -48,6 +48,7 @@ export default function StackNavigator() {
                         }}
                     />
 
+
                     <Stack.Screen
                         name="Login"
                         component={Login}
