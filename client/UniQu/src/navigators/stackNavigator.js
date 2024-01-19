4ff954ceed4130123336e3895c2def04310967ff
tasyaangesti <tasyahestiaangesti@gmail.com>
2024-01-19 20:23:43 +0700

    resolve conflict

--- client/UniQu/src/navigators/stackNavigator.js
+++ client/UniQu/src/navigators/stackNavigator.js
@@ -1,5 +1,8 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator, CardStyleInterpolators } from "@react-navigation/native-stack";
+import {
+  createNativeStackNavigator,
+  CardStyleInterpolators,
+} from "@react-navigation/native-stack";
 import Home from "../screens/home";
 import Login from "../screens/login";
 import Register from "../screens/register";
@@ -10,7 +13,7 @@ import LogoutButton from "../components/logoutButton";
 import TalentDetails from "../screens/talentDetails";
 import ProfileUser from "../screens/profileUser";
 import AllTalent from "../screens/allTalent";
-import MyTabs from "../navigators/tabNavigator"
+import MyTabs from "../navigators/tabNavigator";
 import Chat from "../screens/chat";
 import Booking from "../screens/booking";
 import ListChat from "../screens/list-chat";
@@ -20,89 +23,83 @@ import EditProfileUser from "../screens/editProfileUser";
 const Stack = createNativeStackNavigator();
 
 export default function StackNavigator() {
+  const { isLoggedIn } = useContext(LoginContext);
 
-    const { isLoggedIn } = useContext(LoginContext);
-
-    return (
-        <Stack.Navigator
-            screenOptions={{
-                gestureDirection: "vertical",
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        gestureDirection: "vertical",
+      }}
+    >
+      {isLoggedIn ? (
+        <>
+          <Stack.Screen
+            name="Home"
+            component={MyTabs}
+            options={{
+              headerShown: false,
             }}
-        >
-            {isLoggedIn ? (
-                <>
-                    <Stack.Screen
-                        name="Home"
-                        component={MyTabs}
-                        options={{
-                            headerShown: false,
-                        }}
-                    />
-                    <Stack.Screen name="Edit Profile" component={EditProfileUser} />
-                    <Stack.Screen name="Profile" component={ProfileUser} />
+          />
+          <Stack.Screen name="Edit Profile" component={EditProfileUser} />
+          <Stack.Screen name="Profile" component={ProfileUser} />
 
-                    <Stack.Screen
-                        name="Chat"
-                        component={Chat}
-                        options={{
-                            headerShown: false
-                        }}
-                    />
-                    <Stack.Screen
-                        name="Booking"
-                        component={Booking}
-                        options={{
-                            headerShown: false,
-                        }}
-                    />
+          <Stack.Screen
+            name="Chat"
+            component={Chat}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="Booking"
+            component={Booking}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="All Talent"
+            component={AllTalent}
+            options={{
+              headerShown: false,
+            }}
+          />
 
-                    <Stack.Screen
-                        name="All Talent"
-                        component={AllTalent}
-                        options={{
-                            headerShown: false
-                        }}
-                    />
+          <Stack.Screen
+            name="TalentDetails"
+            component={TalentDetails}
+            options={{
+              headerShown: false,
+            }}
+          />
+        </>
+      ) : (
+        <>
+          <Stack.Screen
+            name="LandingPage"
+            component={LandingPage}
+            options={{
+              headerShown: false,
+            }}
+          />
 
-                    <Stack.Screen
-                        name="TalentDetails"
-                        component={TalentDetails}
-                        options={{
-                            headerShown: false,
-                        }}
-                    />
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{
+              headerShown: false,
+            }}
+          />
 
-
-
-                </>
-            ) : (
-                <>
-                    <Stack.Screen
-                        name="LandingPage"
-                        component={LandingPage}
-                        options={{
-                            headerShown: false,
-                        }}
-                    />
-
-                    <Stack.Screen
-                        name="Login"
-                        component={Login}
-                        options={{
-                            headerShown: false,
-                        }}
-                    />
-
-                    <Stack.Screen
-                        name="Register"
-                        component={Register}
-                        options={{
-                            headerShown: false,
-                        }}
-                    />
-                </>
-            )}
-
-        </Stack.Navigator >
-    );
+          <Stack.Screen
+            name="Register"
+            component={Register}
+            options={{
+              headerShown: false,
+            }}
+          />
+        </>
+      )}
+    </Stack.Navigator>
+  );
 }
