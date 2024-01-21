6d5ed4e76b59442192afe26deae5b2ea66aa5234
Koso55 <radityoprakoso55@gmail.com>
2024-01-21 22:51:53 +0700

    midtrans server, done, need checking

--- server/schemas/transactions.js
+++ server/schemas/transactions.js
@@ -2,6 +2,7 @@ const client = require("../config/configMongo");
 const { GraphQLError } = require("graphql");
 const { ObjectId } = require("mongodb");
 const midtransClient = require("midtrans-client");
+const axios = require("axios");
 
 const { formatDate } = require("../helpers/dateFormat");
 
@@ -17,6 +18,8 @@ const typeDefs = `#graphql
     talentName: String
     userName: String
     paymentId: String
+    orderId: String
+    paymentLink: String
     BookingId: ID
     transactionStatus: String
     paidByAdmin: Boolean
@@ -26,10 +29,6 @@ const typeDefs = `#graphql
   }
 
   input NewTransaction {
-    TalentId: ID
-    UserId: ID
-    talentName: String
-    userName: String
     BookingId: ID
   }
 
@@ -38,7 +37,7 @@ const typeDefs = `#graphql
   }
 
   type Mutation {
-  initPayment(newTransaction:NewTransaction): User
+  initPayment(newTransaction:NewTransaction): Transaction
   }
 
 `;
@@ -72,29 +71,118 @@ const resolvers = {
       try {
         //OLEH TALENT
         const { newTransaction } = args;
+        const { db, authentication } = contextValue;
+        const auth = await authentication();
+
+        // console.log(newTransaction.BookingId, "newTransaction");
+
+        const talentId = auth._id;
+
+        const transaction = await db.collection(COLLECTION_NAME);
+
+        const findBooking = await db.collection("Bookings").findOne({
+          _id: new ObjectId(newTransaction.BookingId),
+        });
+
+        // console.log(findBooking);
+
+        const findTalent = await db.collection("Talents").findOne({
+          _id: new ObjectId(talentId),
+        });
 
         let snap = new midtransClient.Snap({
           isProduction: false,
           serverKey: process.env.MIDTRANS_SERVER_KEY,
         });
 
-        const orderId = `TRX-BKNG-${
-          newTransaction.BookingId
-        }-${Math.random().toString()}-${auth._id}`;
+        const currentDate = new Date();
+
+        const options = {
+          year: "2-digit",
+          month: "2-digit",
+          day: "2-digit",
+          hour: "2-digit",
+          minute: "2-digit",
+          hour12: false,
+        };
+
+        const formattedDate = currentDate.toLocaleDateString("id-Id", options);
+
+        const twoDigitRandom = Math.floor(Math.random() * 90) + 10;
+
+        // const orderId = `TRX-BKNG-${Math.random().toString()}`;
+        // const orderId = `TRX-BKNG-1`;
+        const orderId = `TRX-BKNG-${newTransaction.BookingId}-${auth.username}-${twoDigitRandom}`;
 
         const trxAmount = 500_000;
 
-        const transaction = await snap.createTransaction({
+        const midtransTransaction = await snap.createTransaction({
           transaction_details: {
             order_id: orderId,
-            gross_amount: amount,
+            gross_amount: trxAmount,
           },
+          item_details: [
+            {
+              id: newTransaction.BookingId,
+              price: 500000,
+              quantity: 1,
+              name: "Booking Session with " + findTalent.name,
+            },
+          ],
           customer_details: {
-            email: user.email,
+            first_name: findTalent.name,
+            email: findTalent.email,
           },
         });
 
-        
+        console.log(midtransTransaction, "AAAAAAA");
+
+        console.log(orderId, "orderId");
+
+
+        // const midtransStatusUrl = `https://api.sandbox.midtrans.com/v2/TRX-BKNG-65acbf9036a6e5332ef534ab-JinYoung-92/status`;
+        // // const midtransStatusUrl = `https://api.sandbox.midtrans.com/v2/${orderId}/status`;
+        // console.log(midtransStatusUrl, "midtransStatusUrl");
+
+        // const midtransOptions = {
+        //   method: "GET",
+        //   headers: {
+        //     accept: "application/json",
+        //     Authorization: `Basic ${Buffer.from(
+        //       `${process.env.MIDTRANS_SERVER_KEY}:`
+        //     ).toString("base64")}`,
+        //   },
+        // };
+
+        // const midtransResponse = await axios.get(
+        //   midtransStatusUrl,
+        //   midtransOptions
+        // );
+        // const midtransData = midtransResponse.data;
+
+        // console.log(midtransData, "BBBBBB");
+
+        const createTransaction = await transaction.insertOne({
+          ...newTransaction,
+          TalentId: new ObjectId(talentId),
+          UserId: new ObjectId(findBooking.UserId),
+          BookingId: new ObjectId(newTransaction.BookingId),
+          talentName: findBooking.talentName,
+          userName: findBooking.userName,
+          paymentId: midtransTransaction.token,
+          orderId: orderId,
+          paymentLink: midtransTransaction.redirect_url,
+          transactionStatus: "unpaid",
+          paidByAdmin: false,
+          updatedAt: new Date(),
+          createdAt: new Date(),
+        });
+
+        const findCreatedTransaction = await transaction.findOne({
+          _id: new ObjectId(createTransaction.insertedId),
+        });
+
+        return findCreatedTransaction;
       } catch (error) {
         console.log(error, "INIT_PAYMENT"); // errorHandler next up
         throw new GraphQLError(error.message || "Internal Server Error", {
