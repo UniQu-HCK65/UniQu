33efde9566a3bc6b327b4176e64172133326606f
Koso55 <radityoprakoso55@gmail.com>
2024-01-22 21:38:00 +0700

    added role to login result

--- server/schemas/users.js
+++ server/schemas/users.js
@@ -71,13 +71,14 @@ const typeDefs = `#graphql
     getUserById(userId: ID): ProfileUser
   }
 
-  type Token {
+  type Auth {
     access_token: String
+    role: String
   }
 
   type Mutation {
   register(newUser:NewUser): User
-  login(email:String, password:String): Token
+  login(email:String, password:String): Auth
   editProfile(editUser:EditUser): User
   }
 `;
@@ -469,7 +470,7 @@ const resolvers = {
 
           const access_token = signToken(payload);
 
-          return { access_token };
+          return { access_token, role: payload.role };
         } else if (findTalent) {
           comparedPw = comparePwDecrypted(password, findTalent.password);
 
@@ -489,7 +490,7 @@ const resolvers = {
 
           const access_token = signToken(payload);
 
-          return { access_token };
+          return { access_token, role: payload.role };
         }
       } catch (error) {
         console.log(error, "LOGIN"); // errorHandler next up
